82c701fea5ee4f10f2507230e4481d0c8be942fd
takefumi24 <t_ohba24@yahoo.co.jp>
2020-04-29 02:14:28 +0900

    編集、削除機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,6 +15,22 @@ class PostsController < ApplicationController
   def show
     @post=Post.find(params[:id])
   end
+
+  def edit
+    @post=Post.find(params[:id])
+  end
+
+  def update
+    post=Post.find(params[:id])
+    post.update(post_params)
+    redirect_to post_path(post.id)
+  end
+
+  def destroy
+    post=Post.find(params[:id])
+    post.destroy
+    redirect_to root_path
+  end
   private
   def post_params
     params.require(:post).permit(:title, :content)
